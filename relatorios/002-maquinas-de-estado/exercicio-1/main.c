f1bfb2bfb8f18867ce82f1bf2e54dac82920ada5
Yam Sol Bertamini <yambertamini.sol@gmail.com>
2025-02-24 20:48:42 -0300

    relatorio 2

---
+++ relatorios/002-maquinas-de-estado/exercicio-1/main.c
@@ -0,0 +1,46 @@
+#include <unistd.h>
+#include <stdio.h>
+
+
+#define VERDE    0
+#define AMARELO  1
+#define VERMELHO 2
+
+#define T_VERDE    12
+#define T_AMARELO  3
+#define T_VERMELHO 15
+
+unsigned int estado_atual = VERDE, cor = 0, timer = 0; 
+
+
+int main(void) {
+    for(;;) {
+	switch(estado_atual) {
+	case VERDE:
+	    printf("VERDE\n");
+	    cor = 0;
+	    // _delay_ms(T_VERDE);
+	    sleep(T_VERDE);
+	    estado_atual = AMARELO;
+	    break;
+
+	    case AMARELO:
+	    printf("AMARELO\n");
+	    cor = 1;
+	    // _delay_ms(T_AMARELO);
+	    sleep(T_AMARELO);
+	    estado_atual = VERMELHO;
+	    break;
+	
+	case VERMELHO:
+	    printf("VERMELHO\n");
+	    cor = 2;
+	    // _delay_ms(T_VERMELHO);
+	    sleep(T_VERMELHO);
+	    estado_atual = VERDE;
+	    break;
+	    
+	}
+    }
+    return 0;
+}
